7819812694c4d02f447d64be485ca18452f0a77d
ayanada739 <ayanada739@gmail.com>
2024-08-17 22:27:47 +0300

    Casting Operators - Immediate Execution

--- LINQ 02/Program.cs
+++ LINQ 02/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace LINQ_02
+﻿using System.Collections;
+
+namespace LINQ_02
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            //List<Product> Result = ProductsList.Where(P => P.UnitsInStock == 0).ToList(); //Casting To List
+            //Product[] Result = ProductsList.Where(P => P.UnitsInStock == 0).ToArray(); //Casting To Array
+
+            //Dictionary<long, Product> Result = ProductsList.Where(P => P.UnitsInStock == 0).ToDictionary(P => P.ProductID);
+
+            //Dictionary<long, Product> Result = ProductsList.Where(P => P.UnitsInStock == 0)
+            //                                               .ToDictionary(P => P.ProductID , P => P.ProductName);
+
+            //HashSet<Product> Result = ProductList.Where(P => P.UnitsInStock == 0).ToHashSet();
+
+
+            //ArrayList Obj = new ArrayList()
+            //{
+            //    "Omar" ,
+            //    "Ahmed" ,
+            //    "Mona" ,
+            //    "Aliaa" ,
+            //    1 ,
+            //    2 ,
+            //    3 
+                 
+            //};
+
+            //var Result = Obj.OfType<string>();
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine (item );
+            //}
+
         }
     }
 }
